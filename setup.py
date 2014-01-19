fecc69ce779f8f399c2a911d78f03cafd110c38a
Steven Arcangeli <steven@highlig.ht>
2014-01-19 02:27:47 -0800

    Updating version helper

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 from setuptools import setup, find_packages
-from pypicloud_version import git_version
+from pypicloud_version import git_version, UpdateVersion
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -24,20 +24,26 @@ REQUIREMENTS = [
     'zope.sqlalchemy',
 ]
 
+TEST_REQUIREMENTS = []
+
 if sys.version_info[:2] < (2, 7):
-    REQUIREMENTS.extend(['argparse', 'unittest2'])
+    REQUIREMENTS.extend(['argparse'])
+    TEST_REQUIREMENTS.append('unittest2')
 
 if __name__ == "__main__":
     setup(
         name='pypicloud',
+        version=git_version('pypicloud'),
+        cmdclass={'update_version': UpdateVersion},
         description='Private PyPI backed by S3',
         long_description=README + '\n\n' + CHANGES,
         classifiers=[
             'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
             'Development Status :: 4 - Beta',
-            'Framework :: Pylons',
+            'Framework :: Pyramid',
             'Intended Audience :: System Administrators',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
@@ -64,6 +70,5 @@ if __name__ == "__main__":
             ],
         },
         install_requires=REQUIREMENTS,
-        tests_require=REQUIREMENTS,
-        **git_version()
+        tests_require=REQUIREMENTS + TEST_REQUIREMENTS,
     )
