ca0cf3a5b1ab3fb69fd1b46d492d8fd658f1f0f8
Steven Arcangeli <stevearc@stevearc.com>
2015-04-28 10:56:51 -0700

    Show spinner when logging in

--- pypicloud/static/js/pypicloud.js
+++ pypicloud/static/js/pypicloud.js
@@ -208,20 +208,24 @@ angular.module('pypicloud', ['ui.bootstrap', 'ngRoute', 'angularFileUpload', 'ng
 
 .controller('LoginCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
   $scope.error = false;
+  $scope.loggingIn = false;
 
   if ($window.location.protocol != 'https:' && SECURE_COOKIE) {
     $scope.secureCookieError = true;
   }
 
   $scope.submit = function(username, password) {
+    $scope.loggingIn = true;
     var data = {
       username: username,
       password: password
     };
     $http.post(ROOT + 'login', data).success(function(data, status, headers, config) {
+      $scope.loggingIn = false;
       $scope.error = false;
       window.location = data.next;
     }).error(function(data, status, headers, config) {
+      $scope.loggingIn = false;
       $scope.error = true;
       $scope.errorMsg = 'Username or password invalid';
     });
