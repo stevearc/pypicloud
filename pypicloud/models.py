77f82422d5ac2ef0184c3201fdc1c5a898832a3b
Steven Arcangeli <stevearc@stevearc.com>
2015-06-04 22:55:01 -0700

    Explicitly address every possible request to /simple

--- pypicloud/models.py
+++ pypicloud/models.py
@@ -42,7 +42,7 @@ class Package(object):
 
     def get_url(self, request):
         """ Create path to the download link """
-        return request.app_url('api/package', self.name,
+        return request.app_url('api', 'package', self.name,
                                self.filename)
 
     @property
