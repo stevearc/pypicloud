a1d9180a85cc0ef566c9e5d83cd51416dcbee33d
Steven Arcangeli <stevearc@stevearc.com>
2019-04-04 22:23:49 -0700

    Dropping support for python 3.4
    Tests were failing on something inside SQLAlchemy and MySQL. I don't
    care enough about 3.4 to bother to look into it, so we're giving it the
    boot.

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
             "Programming Language :: Python",
             "Programming Language :: Python :: 2",
             "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Development Status :: 4 - Beta",
