3a003079462bb913cf0b2ab0857dea12cb28440b
Rene Hennig <rene.hennig@noscendo.com>
2019-12-10 16:56:28 +0100

    change tests

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -90,11 +90,15 @@ class TestS3Storage(unittest.TestCase):
         self.assertEqual(parts.hostname, "mybucket.s3.amazonaws.com")
         self.assertEqual(parts.path, "/" + self.storage.get_path(package))
         query = parse_qs(parts.query)
-        self.assertItemsEqual(query.keys(), ["Expires", "Signature", "AWSAccessKeyId"])
-        self.assertTrue(int(query["Expires"][0]) > time.time())
-        self.assertEqual(
-            query["AWSAccessKeyId"][0], self.settings["storage.aws_access_key_id"]
-        )
+        self.assertItemsEqual(query.keys(), [
+            "X-Amz-Algorithm",
+            "X-Amz-Credential",
+            "X-Amz-Date",
+            "X-Amz-Expires",
+            "X-Amz-SignedHeaders",
+            "X-Amz-Signature"
+        ])
+        self.assertEqual(query["X-Amz-Date"][0], datetime.datetime.utcnow().strftime("%Y%m%dT%H%M%SZ"))
 
     def test_delete(self):
         """ delete() should remove package from storage """
@@ -133,10 +137,26 @@ class TestS3Storage(unittest.TestCase):
         match = re.match(pattern, key.key)
         self.assertIsNotNone(match)
 
-    def test_create_bucket(self):
+    def test_create_bucket_eu(self):
         """ If S3 bucket doesn't exist, create it """
-        settings = {"storage.bucket": "new_bucket", "storage.region_name": "us-east-1"}
+        settings = {
+            "storage.bucket": "new_bucket",
+            "storage.region_name": "eu-central-1",
+            "signature_version": "s3v4"
+        }
         S3Storage.configure(settings)
+
+        bucket = self.s3.Bucket("new_bucket")
+        bucket.load()
+
+    def test_create_bucket_us(self):
+        """ If S3 bucket doesn't exist, create it """
+        settings = {
+            "storage.bucket": "new_bucket",
+            "storage.region_name": "us-west-1"
+        }
+        S3Storage.configure(settings)
+
         bucket = self.s3.Bucket("new_bucket")
         bucket.load()
 
