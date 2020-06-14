d88016905b63859fb029b57aa452bf0f90a96df6
Steven Arcangeli <stevearc@stevearc.com>
2020-06-14 13:53:10 -0700

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ EXTRAS["doc"] = ["numpydoc", "sphinx", "sphinx_rtd_theme"]
 if __name__ == "__main__":
     setup(
         name="pypicloud",
-        version="1.1.0",
+        version="1.1.1",
         description="Private PyPI backed by S3",
         long_description=README + "\n\n" + CHANGES,
         classifiers=[
