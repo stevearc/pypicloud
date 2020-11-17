1e774a1581d7e5e4d213e0d0037086c754b1c820
Steven Arcangeli <stevearc@stevearc.com>
2020-11-16 21:26:04 -0800

    Bump version: 1.1.6 → 1.1.7

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ project = u'pypicloud'
 copyright = u'2013, Steven Arcangeli'
 github_user = u'stevearc'
 
-release = '1.1.6'
+release = '1.1.7'
 version = '.'.join(release.split('.')[:2])
 
 exclude_patterns = ['_build']
