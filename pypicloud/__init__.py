2d8701820717f611b0a99f12a51e9eac2e0a5466
Steven Arcangeli <stevearc@stevearc.com>
2021-06-09 14:41:20 -0700

    Bump version: 1.2.2 → 1.2.3

--- pypicloud/__init__.py
+++ pypicloud/__init__.py
@@ -13,7 +13,7 @@ from pyramid_beaker import session_factory_from_settings
 from .locator import FormattedScrapingLocator, SimpleJsonLocator
 from .route import Root
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 LOG = logging.getLogger(__name__)
 
 
