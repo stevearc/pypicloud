b564680d532e40c1922a19fc881a406ffe92091b
Steven Arcangeli <stevearc@stevearc.com>
2021-10-16 09:36:44 -0700

    Bump version: 1.3.1 → 1.3.2

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ EXTRAS["server"] = ["waitress"]
 if __name__ == "__main__":
     setup(
         name="pypicloud",
-        version="1.3.1",
+        version="1.3.2",
         description="Private PyPI backed by S3",
         long_description=README + "\n\n" + CHANGES,
         classifiers=[
